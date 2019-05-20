2ed6c496fe73cd7104ce5cbe3ef574cb4c8429ee
khz_df <zhaoanhua6763@dingtalk.com>
2019-05-20 10:23:44 +0800

    [tools] VS生成工程提交

---
+++ course/WinDriver/tools/tools-MFC/tools-MFC.h
@@ -0,0 +1,32 @@
+﻿
+// tools-MFC.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CtoolsMFCApp:
+// 有关此类的实现，请参阅 tools-MFC.cpp
+//
+
+class CtoolsMFCApp : public CWinApp
+{
+public:
+	CtoolsMFCApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CtoolsMFCApp theApp;
